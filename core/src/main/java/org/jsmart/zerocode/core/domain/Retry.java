fa3f076f2d2e3fdb7c1940289f54342ae4dbc9f3
Jordan Liebe <jordan.liebe@outlook.com>
2025-03-02 13:32:00 -0800

    Extended retrial code to support exponential backoff

--- core/src/main/java/org/jsmart/zerocode/core/domain/Retry.java
+++ core/src/main/java/org/jsmart/zerocode/core/domain/Retry.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Retry {
     private Integer max;
     private Integer delay;
+    private String strategy; // New field for "fixed" or "exponential" strategy
     private List<String> withSteps;
 
     public Integer getMax() {
@@ -15,16 +16,19 @@ public class Retry {
         return delay;
     }
 
+    public String getStrategy() {
+        return strategy;
+    }
+
     public List<String> getWithSteps() {
         return withSteps;
     }
     public Retry() {}
 
-    public Retry(Integer max, Integer delay, List<String> withSteps) {
+    public Retry(Integer max, Integer delay, String strategy, List<String> withSteps) {
         this.max = max;
         this.delay = delay;
+        this.strategy = strategy;
         this.withSteps = withSteps;
     }
-
-
 }
